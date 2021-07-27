38b5fd9b52eaee71b6fc66fb8b60a58d5e4d5c82
Đỗ Xuân Nam <doxuannam1711@gmail.com>
2021-07-27 10:30:52 +0700

    Add Happy Emoji

---
+++ audio.js
@@ -0,0 +1,6 @@
+window.addEventListener("mouseover",play);
+window.addEventListener("click",play);
+function play(){ 
+    document.getElementById("audio").play();
+}
+alert("Click anywhere to enable music");
